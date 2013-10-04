339970e1494e2abc913ab0372097f8cc5382472a
Thiago Yukio Kikuchi Oliveira <stratust@gmail.com>
2013-10-04 00:51:56 -0400

    Added accessor tests

--- t/lib/TestsFor/Bio/Moose/IgBlast.pm
+++ t/lib/TestsFor/Bio/Moose/IgBlast.pm
@@ -1,16 +1,42 @@
 package TestsFor::Bio::Moose::IgBlast;
     use Test::Class::Moose;
+    with 'Test::Class::Moose::Role::AutoUse';
     use Bio::Moose::IgBlast;
+    
+    BEGIN {
+        has 'obj' => (
+            is            => 'rw',
+            isa           => 'Object',
+            documentation => 'Hold object for test purposes',
+        );
+    }
+    
+    sub extra_constructor_args {}
 
-    sub test_construction {
-        my ( $test, $report ) = @_;
-        my $obj = Bio::Moose::IgBlast->new(
+    sub test_setup{
+         my $test = shift;
+         $test->obj($test->class_name->new(
             database              => 'filename',
             domain_classification => 'kabat',
             molecule              => 'N',
             query_id              => "id1",
-            version               => '1'
-        );
-        isa_ok $obj, 'Bio::Moose::IgBlast';
+            version               => '2.26+'
+             )
+         );
+    }
+
+    sub test_construction {
+        my ( $test, $report ) = @_;
+        isa_ok $test->obj, 'Bio::Moose::IgBlast';
+    }
+
+    sub test_accessors {
+        my ( $test, $report ) = @_;
+        my $o = $test->obj;
+        is( $o->database,              'filename', "Read Database" );
+        is( $o->domain_classification, 'kabat',    "Read Domain" );
+        is( $o->molecule,              'N',        "Read molecule" );
+        is( $o->query_id,              'id1',      "Read query_id" );
+        is( $o->version,               '2.26+',    "Read version" );
     }
 1
