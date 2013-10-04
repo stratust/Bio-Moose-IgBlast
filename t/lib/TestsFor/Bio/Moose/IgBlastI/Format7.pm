6f12d0fe63ced33f2dbede30c71a5020eee56295
Thiago Yukio Kikuchi Oliveira <stratust@gmail.com>
2013-10-04 01:33:16 -0400

    Added read tests

--- t/lib/TestsFor/Bio/Moose/IgBlastI/Format7.pm
+++ t/lib/TestsFor/Bio/Moose/IgBlastI/Format7.pm
@@ -1,31 +1,69 @@
 package TestsFor::Bio::Moose::IgBlastI::Format7;
     use Test::Class::Moose;
+    with 'Test::Class::Moose::Role::AutoUse';
     use Bio::Moose::IgBlastI::Format7;
+
+    BEGIN {
+        has 'obj' => (
+            is            => 'rw',
+            isa           => 'Object',
+            documentation => 'Hold object for test purposes',
+        );
+    }
+
+    sub test_setup {
+        my $test = shift;
+        $test->obj(
+            $test->class_name->new(
+                'file' =>
+'t/data/format7/input_files/1.0/IgBlast_heavy_lotta_1.0-7_all.out',
+            )
+        );
+    }
 
     sub test_construction {
         my ( $test, $report ) = @_;
 
-        can_ok 'Bio::Moose::IgBlastI::Format7', 'new';
-
-        my $obj = Bio::Moose::IgBlastI::Format7->new( 'file' => '', );
-
-        isa_ok $obj, 'Bio::Moose::IgBlastI::Format7';
+        isa_ok $test->obj, 'Bio::Moose::IgBlastI::Format7';
     }
 
     sub test_parse_version_1_0 {
         my ( $test, $report ) = @_;
 
-        my $obj = Bio::Moose::IgBlastI::Format7->new(
-            'file'  => 't/data/format7/input_files/1.0/IgBlast_heavy_lotta_1.0-7_all.out',
+        can_ok $test->obj,(qw/features next_feature/);
+        
+        isa_ok $test->obj->features, 'ARRAY';
+
+        my $o = $test->obj->next_feature;
+        isa_ok $o, 'Bio::Moose::IgBlast';
+
+        # Test attributes
+        is(
+            $o->database,
+            'database/human_gl_V database/human_gl_D database/human_gl_J',
+            "Read Database"
         );
-
-        can_ok $obj,(qw/features next_feature/);
+        is( $o->domain_classification, 'kabat', "Read Domain" );
+        is( $o->molecule,              'N',     "Read molecule" );
+        is( $o->query_id,              '3A_P94_LVH_G3-IgGinternal.ab1',   "Read query_id" );
+        is( $o->version,               '2.2.27+', "Read version" );
         
-        isa_ok $obj->features, 'ARRAY';
-
-        my $first = $obj->next_feature;
         
-        isa_ok $first, 'Bio::Moose::IgBlast';
+        # Rearrangement Summary
+        my $r = $o->rearrangement_summary;
+        isa_ok $r, 'Bio::Moose::IgBlast::Rearrangement';
+
+        # Junction Details
+        my $j = $o->junction_details;
+        isa_ok $j, 'Bio::Moose::IgBlast::Junction';
+
+        # Alignments
+        my $a = $o->alignments;
+        isa_ok $a, 'Bio::Moose::IgBlast::Alignment';
+
+        # Hit Table
+        my $h = $o->hit_table;
+        isa_ok $h, 'Bio::Moose::IgBlast::HitTable';
 
     }
 
