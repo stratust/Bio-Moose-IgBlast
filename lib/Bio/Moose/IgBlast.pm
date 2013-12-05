a756861c6b033f5ea06faac73ebc4b3e4d5d3d91
Thiago Yukio Kikuchi Oliveira <stratust@gmail.com>
2013-12-05 17:38:26 -0500

    infer_cdr3 method changed to Igblast class

--- lib/Bio/Moose/IgBlast.pm
+++ lib/Bio/Moose/IgBlast.pm
@@ -39,6 +39,94 @@ class Bio::Moose::IgBlast {
     # store any information you want in this part
     has misc     => ( is => 'rw', isa => 'Any' );
     has init_pos => ( is => 'ro', isa => 'Int' );
+
+
+    method infer_CDR3_nt {
+        my $cdr3_seq = "N/A";
+
+        my $query = $self->rendered_alignment->query;
+
+        if ( $query->sub_regions_sequence->has_FWR3 ) {
+
+            my $r = quotemeta $query->sub_regions_sequence->FWR3;
+            
+            # If heavy chain
+            if ( $self->rearrangemet->top_V_match =~ /IGH/i ) {
+            
+                if ( $query->sequence =~ /($r)(\S+)TGGGGC/i ) {
+                    $cdr3_seq = $2 . "|";
+                }
+                elsif ( $query->sequence =~ /($r)(\S+)/i ) {
+                    $cdr3_seq = $2;
+                }
+            }
+            # If light chain
+            elsif ( $self->rearrangemet->top_V_match =~ /IGL/i ) {
+            
+                if ( $query->sequence =~ /($r)(\S+)TCCTGT/i ) {
+                    $cdr3_seq = $2 . "|";
+                }
+                elsif ( $query->sequence =~ /($r)(\S+)/i ) {
+                    $cdr3_seq = $2;
+                }
+            }
+        }
+        return $cdr3_seq;
+    }
+
+
+    method infer_CDR3_nt_length {
+        my $cdr3_seq = $self->infer_CDR3_nt;
+        my $len = "N/A";
+        if ($cdr3_seq ne "N/A"){
+            $cdr3_seq =~ s/\|//g;
+            $len = length $cdr3_seq;
+        }
+        return $len;
+    }
+
+
+    method infer_CDR3_aa {
+        my $cdr3_seq = "N/A";
+        my $query    = $self->rendered_alignment->query;
+
+        if ( $query->sub_regions_translation->has_FWR3 ) {
+      
+            my $r = quotemeta $query->sub_regions_translation->FWR3;
+ 
+            if ( $self->rearrangemet->top_V_match =~ /IGH/i ) {
+               if ( $query->translation =~ /($r)(\S+)WG/i ) {
+                    $cdr3_seq = $2 . "|";
+                }
+                elsif ( $query->translation =~ /($r)(\S+)/i ) {
+                    $cdr3_seq = $2;
+                }
+            }
+
+            # If light chain
+            elsif ( $self->rearrangemet->top_V_match =~ /IGL/i ) {
+                if ( $query->translation =~ /($r)(\S+)SC)/i ) {
+                    $cdr3_seq = $2 . "|";
+                }
+                elsif ( $query->translation =~ /($r)(\S+)/i ) {
+                    $cdr3_seq = $2;
+                }
+            }
+        }
+        return $cdr3_seq;
+    }
+
+
+    method infer_CDR3_aa_length {
+        my $cdr3_seq = $self->infer_CDR3_aa;
+        my $length = "N/A";
+        if ($cdr3_seq ne "N/A"){
+            $cdr3_seq =~ s/\|//g;
+            $length = length $cdr3_seq;
+        }
+        return $length;
+    }
+
 }
 
  # ABSTRACT: turns baubles into trinkets
