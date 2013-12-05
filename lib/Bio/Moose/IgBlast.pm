de37ca347c6aa84c8331adea5d9a5e3c70bd3b0d
Thiago Yukio Kikuchi Oliveira <stratust@gmail.com>
2013-12-05 18:17:59 -0500

    Corrected infer CDR3 to work with kappa and lambda chains

--- lib/Bio/Moose/IgBlast.pm
+++ lib/Bio/Moose/IgBlast.pm
@@ -63,7 +63,7 @@ class Bio::Moose::IgBlast {
                 }
 
                 # If light chain
-                elsif ( $self->rearrangement_summary->top_V_match =~ /IGL/i ) {
+                elsif ( $self->rearrangement_summary->top_V_match =~ /IG[LK]/i ) {
 
                     if ( $query->sequence =~ /($r)(\S+)TCCTGT/i ) {
                         $cdr3_seq = $2 . "|";
@@ -109,7 +109,7 @@ class Bio::Moose::IgBlast {
                 }
 
                 # If light chain
-                elsif ( $self->rearrangement_summary->top_V_match =~ /IGL/i ) {
+                elsif ( $self->rearrangement_summary->top_V_match =~ /IG[LK]/i ) {
                     if ( $query->translation =~ /($r)(\S+)SC)/i ) {
                         $cdr3_seq = $2 . "|";
                     }
